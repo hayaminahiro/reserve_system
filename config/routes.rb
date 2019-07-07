bb447b9ded0ea3cb807e4dc8abf294033bb69814
EC2 Default User <ec2-user@ip-172-31-47-11.us-east-2.compute.internal>
2019-07-07 10:04:19 +0000

    Finish session-login

--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,15 @@
 Rails.application.routes.draw do
   root 'static_pages#top'
+ 
+  # サインアップ
   get '/signup',  to: 'users#new'
   post '/signup', to: 'users#create'
+  
+  # ログイン
+  get '/login', to: 'sessions#new'
+  post '/login', to: 'sessions#create'
+  
+  # ログアウト
+  delete '/logout', to: 'sessions#destroy'
   resources :users
 end
