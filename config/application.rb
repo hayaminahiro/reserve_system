311d485cb379bd62d16859e020784dfcdf32b02b
EC2 Default User <ec2-user@ip-172-31-47-11.us-east-2.compute.internal>
2019-08-02 02:39:16 +0000

    heroku-time-zone

--- config/application.rb
+++ config/application.rb
@@ -13,6 +13,7 @@ module AttendanceSystem
     config.time_zone = 'Tokyo'
     config.active_record.default_timezone = :local
     config.i18n.default_locale = :ja # デフォルトのlocaleを日本語(:ja)にする
+    # herokuでもタイムゾーン変更する
     
     #　#　以下の記述を追記する(設定必須)
     config.i18n.load_path += Dir[Rails.root.join('config', 'locales', '**', '*.{rb,yml}').to_s]
