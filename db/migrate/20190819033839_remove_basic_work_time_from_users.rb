b619b451de43d560b7a0402cfee870ae3a0c45df
Hayato Saito <omimisama@gmail.com>
2019-11-23 08:11:38 +0900

    heroku_issue

--- db/migrate/20190819033839_remove_basic_work_time_from_users.rb
+++
@@ -1,5 +0,0 @@
-class RemoveBasicWorkTimeFromUsers < ActiveRecord::Migration[5.2]
-  def change
-    remove_column :users, :basic_work_time, :datetime
-  end
-end
--- db/migrate/20190819034104_add_basic_work_time_to_users.rb
+++ db/migrate/20190819034104_add_basic_work_time_to_users.rb
@@ -3,3 +3,8 @@ class AddBasicWorkTimeToUsers < ActiveRecord::Migration[5.2]
     add_column :users, :basic_work_time, :datetime, default: Time.zone.parse('2019-07-01 07:30:00')
   end
 end
+
+# heroku pg:psql
+# ;
+#
+#
